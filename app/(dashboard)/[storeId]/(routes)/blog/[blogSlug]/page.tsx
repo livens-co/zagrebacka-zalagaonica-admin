37c904e31d7cb6cdc33c1c7263c4e1aeb0c9af91
dcvijetovic <dominik@mintandlime.eu>
2023-10-19 14:29:00 +0200

    blog upute

--- app/(dashboard)/[storeId]/(routes)/blog/[blogSlug]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/blog/[blogSlug]/page.tsx
@@ -1,5 +1,5 @@
-import prismadb from '@/lib/prismadb';
-import { ArticleForm } from './components/article-form';
+import prismadb from "@/lib/prismadb";
+import { ArticleForm } from "./components/article-form";
 
 const ArticlePage = async ({
   params,
@@ -8,7 +8,7 @@ const ArticlePage = async ({
 }) => {
   const article = await prismadb.blog.findUnique({
     where: {
-      blogSlug: params.blogSlug, 
+      blogSlug: params.blogSlug,
     },
   });
 
@@ -16,6 +16,20 @@ const ArticlePage = async ({
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <ArticleForm initialData={article} />
+      </div>
+      <div className="flex-col">
+        <h3>Upute za blog</h3>
+        <ul>
+          <li>
+            <span className="font-bold">h1</span> #
+          </li>
+          <li>
+            <span className="font-bold">h2</span> ##
+          </li>
+          <li>
+            <span className="font-bold">h3</span> ###
+          </li>
+        </ul>
       </div>
     </div>
   );
